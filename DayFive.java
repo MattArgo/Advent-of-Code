5638b70de562584a4ac8b7f342196319afd915bc
MattArgo <34251146+MattArgo@users.noreply.github.com>
2017-12-05 23:19:00 -0500

    Update DayFive.java
    Cleaned up code

--- DayFive.java
+++ DayFive.java
@@ -6,33 +6,26 @@ public class DayFive{
 		try {
 			BufferedReader br = new BufferedReader(new FileReader("inputDay5.txt"));
 			StringBuilder sb = new StringBuilder();
-			String line = br.readLine();
-			
-			ArrayList<Integer> ints = new ArrayList();
-			
+			String line = br.readLine();			
+			ArrayList<Integer> ints = new ArrayList();			
 			while(line != null){
 					ints.add(Integer.parseInt(line.substring(0,line.length())));
-					System.out.println(Integer.parseInt(line.substring(0,line.length())));
-					
-					
+					System.out.println(Integer.parseInt(line.substring(0,line.length())));	
 					line = br.readLine();
 			}
-
 			int position = 0;
 			int move;
 			int numMoves = 0;
-			
 			while((position >= 0) && (position < ints.size())){
 				move = ints.get(position);
 				ints.set(position, move + 1);
 				position += move;
 				numMoves++;
 			}
-			
 			System.out.println(numMoves);
 		}
 		catch(Exception e){
-			//System.out.println(e.getMessage());
+			System.out.println(e.getMessage());
 		}
 	}
 
