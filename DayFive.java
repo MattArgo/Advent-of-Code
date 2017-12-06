33117cdecf93028499f7ca447da1cb9501a5cca0
MattArgo <34251146+MattArgo@users.noreply.github.com>
2017-12-05 22:41:15 -0500

    Add files via upload

---
+++ DayFive.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.*;
+public class DayFive{
+	public static void main(String[] args){
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("inputDay5.txt"));
+			StringBuilder sb = new StringBuilder();
+			String line = br.readLine();
+			
+			ArrayList<Integer> ints = new ArrayList();
+			
+			while(line != null){
+					ints.add(Integer.parseInt(line.substring(0,line.length())));
+					System.out.println(Integer.parseInt(line.substring(0,line.length())));
+					
+					
+					line = br.readLine();
+			}
+
+			int position = 0;
+			int move;
+			int numMoves = 0;
+			
+			while((position >= 0) && (position < ints.size())){
+				move = ints.get(position);
+				ints.set(position, move + 1);
+				position += move;
+				numMoves++;
+			}
+			
+			System.out.println(numMoves);
+		}
+		catch(Exception e){
+			//System.out.println(e.getMessage());
+		}
+	}
+
+}
